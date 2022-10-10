e9b13af6b07e9101c71cce7e54200bb91bd22b08
Benedikt Holmes <20526499+benediktholmes@users.noreply.github.com>
2022-10-10 10:10:56 +0200

    Full project

--- tests/test_module_level_function.py
+++ tests/test_module_level_function.py
@@ -1,15 +1,16 @@
 import pytest
 
-from example_google import module_level_function
+from src.example_google import module_level_function
 
 
 def test_module_level_function():
-  assert module_level_function('a','b')
-  assert module_level_function(0,'a')
-  assert module_level_function(0,0)
-  assert module_level_function('a')
-  assert module_level_function(5)
-  
+    assert module_level_function('a', 'b')
+    assert module_level_function(0,'a')
+    assert module_level_function(0)
+    assert module_level_function('a')
+    assert module_level_function(5)
+
+
 def test_module_level_function_two():
-  with pytest.raises(ValueError):
-    assert module_level_function('b','b')
+    with pytest.raises(ValueError):
+        assert module_level_function('b', 'b')
